ea47efa3a93338d941721dc477dc038025c483f3
Prateek Chandan <prateekchandan5545@gmail.com>
2015-09-02 07:34:59 +0530

    from email

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => env('FROM_EMAIL', 'support@getpickup.in'), 'name' => env('FROM_NAME', 'Team Pickup')],
 
 	/*
 	|--------------------------------------------------------------------------
