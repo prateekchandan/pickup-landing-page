c3a73772b9ca90e38cd0f5f472c4c96fbfff886c
Prateek Chandan <prateekchandan5545@gmail.com>
2015-09-29 14:16:50 +0530

    remove creation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,19 +41,9 @@ class HomeController extends Controller {
 			$num = intval(Input::get('click'));
 			if($num>=1 && $num<=7){
 				$click = ClickCount::find($num);
-				if(is_null($click)){
-					ClickCount::insert([
-						['id'=>1,'description'=>'Pickup Logo','type'=>'ICICI Campaign'],
-						['id'=>2,'description'=>'JUST "CLICK" AND BOOK YOUR FIRST RIDE FOR FREE!','type'=>'ICICI Campaign'],
-						['id'=>3,'description'=>'Banner Image of girl','type'=>'ICICI Campaign'],
-						['id'=>4,'description'=>'AND SO, WE CREATED PICKUP','type'=>'ICICI Campaign'],
-						['id'=>5,'description'=>'FOR ICICI EMPLOYEES, THE FIRST RIDE IS FREE.','type'=>'ICICI Campaign'],
-						['id'=>6,'description'=>'VISIT WEBSITE','type'=>'ICICI Campaign'],
-						['id'=>7,'description'=>'To make your daily commute easier, just click here and we’ll take it from there.','type'=>'ICICI Campaign']
-						]);
-					$click = ClickCount::find($num);
+				if(!is_null($click)){
+					$click->plus_one();
 				}
-				$click->plus_one();
 			}
 		}
 		if(Agent::isMobile())
