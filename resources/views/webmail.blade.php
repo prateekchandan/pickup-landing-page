ee7bba18ba1be528093806660f2b09cb3e92ef16
Prateek Chandan <prateekchandan5545@gmail.com>
2015-09-29 12:48:35 +0530

    Added track codes

--- resources/views/webmail.blade.php
+++ resources/views/webmail.blade.php
@@ -27,7 +27,7 @@ a.normal:hover,a.normal:focus{
                             <tr>
                                 <td valign="top"> 
                                     <!--top links-->
-                                    <table cellspacing="0" border="0" cellpadding="0" width="624" style="background-color:#E0EBEA;padding:5px">
+                                    <table cellspacing="0" border="0" cellpadding="0" width="624" style="background-color:#E0EBEA;padding:5px;margin:0px">
                                         <tr>
                                             <td valign="middle" width="221">
                                                 <p style="font-size: 12px; font-family: Helvetica, Arial, sans-serif; color: #333; margin: 0px;">Pickup Is Here!</p>
@@ -53,10 +53,10 @@ a.normal:hover,a.normal:focus{
                                     <table cellspacing="0" border="0" cellpadding="0" width="624">
                                         <tr>
                                             <td id="issue" valign="top" style="width: 109px; height: 109px;vertical-align: middle;" bgcolor="#FFFFFF">
-                                                <a style="color:fff" class="normal" href="http://getpickup.in"><img align="right" src="{{asset('assets/img/icon/ms-icon-310x310.png')}}" style="width: 100%;margin: auto;max-height: 100px;"></a>
+                                                <a style="color:fff" class="normal" href="http://getpickup.in?click=1"><img align="right" src="{{asset('assets/img/icon/ms-icon-310x310.png')}}" style="width: 100%;margin: auto;max-height: 100px;"></a>
                                             </td>
                                             <td style="background-color:#0D052E;padding:10px;min-height:120px;vertical-align:middle">
-                                                <h1 style="color:#fff!important;margin:0px;font-weight:normal;font-size:1.3em;font-family:Helvetica,Arial,sans-serif;height: 120px;vertical-align: middle;display: table-cell;">NOW GO HOME IN A CAB FOR JUST INR 99!<br><br>JUST <a style="color:fff" class="normal" href="http://getpickup.in"><u>CLICK</u></a> AND BOOK YOUR FIRST RIDE FOR FREE!</h1>
+                                                <h1 style="color:#fff!important;margin:0px;font-weight:normal;font-size:1.3em;font-family:Helvetica,Arial,sans-serif;height: 120px;vertical-align: middle;display: table-cell;">NOW GO HOME IN A CAB FOR JUST INR 99!<br><br>JUST <a style="color:fff" class="normal" href="http://getpickup.in?click=2"><u>CLICK</u></a> AND BOOK YOUR FIRST RIDE FOR FREE!</h1>
                                                 
                                             </td>
                                         </tr>
@@ -81,14 +81,14 @@ a.normal:hover,a.normal:focus{
                                     <table cellspacing="0" border="0" cellpadding="0" width="624">
                                         <tr>
                                             <td>
-                                                <p style="font-size: 17px; line-height: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #333; margin: 0px;"><a style="color:fff" class="normal" href="http://getpickup.in"><img src="{{asset('email/images/img1.jpg')}}" height="286" alt="image dsc" style="" width="622" /></a></p>
+                                                <p style="font-size: 17px; line-height: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #333; margin: 0px;"><a style="color:fff" class="normal" href="http://getpickup.in?click=3"><img src="{{asset('email/images/img1.jpg')}}" height="286" alt="image dsc" style="" width="622" /></a></p>
                                                 <p style="height:5px;margin:0px"></p>
                                                 <div style="font-family: Helvetica, Arial, sans-serif;padding:10px;background-color:#FFBF47;color:#0D052E;text-align:center;line-height: 25px;padding-left: 30px;padding-right: 30px;">
                                                     <p>Most times, going home after a long day at work can be tiring, or expensive, or both. </p>
                                                     <p>At Pickup, we believe that public transport, being economical and eco-friendly, would be the best way to travel</p>
                                                     <p>However, this system is often overcrowded and limited to main roads. Options like Uber and Ola Cabs give you all the comfort, but charge you a price, that would become too expensive for daily use. </p>
                                                     <p>We wanted to take the convenience of BEST buses with the comfort of options like Uber, and add a little twist. </p>
-                                                    <p><a class="normal" href="http://getpickup.in"><b><u>AND SO, WE CREATED PICKUP. </u></b></a></p>
+                                                    <p><a class="normal" href="http://getpickup.in?click=4"><b><u>AND SO, WE CREATED PICKUP. </u></b></a></p>
                                                     <p>PickUp is an on demand carpooling platform that allows you to book a ride to and from your home and office at affordable price as low as Rs. 99. </p>
                                                     <p>You book a ride through our app, and If we find other people on your way, we add them to your ride. If we don’t, you still ride at the same cost. </p>
                                                     <p>An upper limit of 3 people per ride ensures privacy and comfort, while our economical fares gives you a low-cost journey.</p>
@@ -108,12 +108,12 @@ a.normal:hover,a.normal:focus{
                                     <table cellspacing="0" border="0" cellpadding="0" width="624">
                                         <tr>
                                             <td>
-                                                <h3 style="font-size: 1.2em; font-family: Helvetica, Arial, sans-serif; color: #000000 !important; margin: 0px;background-color:#FFBF47;padding:20px;text-align:center">FOR ICICI EMPLOYEES, THE <a style="color:fff" class="normal" href="http://getpickup.in">FIRST RIDE IS FREE</a>.</h3>
+                                                <h3 style="font-size: 1.2em; font-family: Helvetica, Arial, sans-serif; color: #000000 !important; margin: 0px;background-color:#FFBF47;padding:20px;text-align:center">FOR ICICI EMPLOYEES, THE <a style="color:fff" class="normal" href="http://getpickup.in?click=5">FIRST RIDE IS FREE</a>.</h3>
                                                 <p style="height:10px;margin:0px"></p>
                                                 <table cellspacing="0" border="0" cellpadding="8" width="100%">
                                                     <tr>
                                                         <td valign="top" style="text-align:center;background-color: #FFBF47;border-right: 5px solid #fff">
-                                                            <a style="color:fff" class="normal" href="http://getpickup.in">
+                                                            <a style="color:fff" class="normal" href="http://getpickup.in?click=6">
                                                             <p style="font-size: 17px; line-height: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #333; margin: 0px;"><img src="{{asset('email/images/img2.jpg')}}" alt="img2" style=";" width="100" /></p>
                                                             <h4 style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: #333 !important; margin: 12px 0px;">Visit Website</h4>
                                                             </a>
@@ -147,7 +147,7 @@ a.normal:hover,a.normal:focus{
                                         <tr>
                                             <td>
                                                 <p style="color: #fff !important; margin: 0px; font-weight: normal; font-size: 16px; font-family: Helvetica, Arial, sans-serif;background-color:#0D052E;padding:10px;text-align:center;line-height:25px">We’re kick-starting operations from this Wednesday (30, September).  <br>To make your daily commute easier, just <br>
-                                                <a style="color:fff" class="normal" href="http://getpickup.in"><u>click here</u></a><br>and we’ll take it from there.</p>
+                                                <a style="color:fff" class="normal" href="http://getpickup.in?click=7"><u>click here</u></a><br>and we’ll take it from there.</p>
                                                 <p style="font-size: 17px; line-height: 24px; font-family: Georgia, 'Times New Roman', Times, serif; color: #333; margin: 0px;"><img src="{{asset('email/images/spacer-ten.jpg')}}" height="10" width="624" /></p>
                                             </td>
                                         </tr>
