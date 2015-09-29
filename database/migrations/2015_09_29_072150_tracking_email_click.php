ad43635479b374b8a40a1a248d054228b17c4215
Prateek Chandan <prateekchandan5545@gmail.com>
2015-09-29 12:57:37 +0530

    Added count table

---
+++ database/migrations/2015_09_29_072150_tracking_email_click.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TrackingEmailClick extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Schema::dropIfExists('click_count');
+        Schema::create('click_count', function($table)
+        {
+            $table->integer('id');
+            $table->primary('id');
+            $table->string('description',1000)->default("");
+            $table->integer('count')->default(0);
+            $table->string('type',1000)->default("");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
